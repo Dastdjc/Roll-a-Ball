62410cc01a27637ddc722e60dd22040d6df78c25
Dastdjc <90397187+Dastdjc@users.noreply.github.com>
2021-09-15 17:29:33 +0200

    Update playerController.cs

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -7,6 +7,9 @@ public class playerController : MonoBehaviour
 {
 
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +20,15 @@ public class playerController : MonoBehaviour
     void OnMove(InputValue momentValue)
     {
         Vector2 momentVector = momentValue.Get<Vector2>();
+
+        movementX = momentVector.x;
+        movementY = momentVector.y;
     }
 
     void  FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+
+        rb.AddForce(movement);
     }
 }
